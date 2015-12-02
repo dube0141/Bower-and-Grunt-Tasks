d2aeffbf44c91957143b16bd6e361d8e352c5a81
Gabriel Dubé <Gabriel@Gabriels-MacBook-Pro.local>
2015-12-02 14:03:43 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$("#popup").click(function () {
+		var txt = "";
+		txt += "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+		txt += "<p>Browser Name: " + navigator.appName + "</p>";
+		txt += "<p>Browser Version: " + navigator.appVersion + "</p>";
+		txt += "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+		txt += "<p>Browser Language: " + navigator.language + "</p>";
+		txt += "<p>Browser Online: " + navigator.onLine + "</p>";
+		txt += "<p>Platform: " + navigator.platform + "</p>";
+		txt += "<p>User-agent header: " + navigator.userAgent + "</p>";
+		toastr.info(txt);
+	});
+});
